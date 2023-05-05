de3bef997d089985fa898791308898cad0a5a2e4
ss13 <ssa0618@yandex.ru>
2023-05-05 19:18:10 +0300

    RUB

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,10 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
 class MobileBankApiTestV1 {
     @Test
@@ -16,6 +18,8 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .contentType(ContentType.JSON)
+              .body("",hasSize(3));
     }
 }
